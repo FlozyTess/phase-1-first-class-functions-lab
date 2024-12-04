1a88a3d9bd59d1d97d0942932f2efc32cd6c3224
FlozyTess <cecilrency23@gmail.com>
2024-12-05 02:19:27 +0300

    implemented an array with functions

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution in this file!
+//returnFirstTwoDrivers
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);
+};
+console.log =(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']))
+//returnLastTwoDrivers
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  console.log(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
